772d2873d0067581c0f7692cbd2d9443b33766d3
kareembadrsaber <kimobadrsaber@gmail.com>
2023-10-20 12:13:37 -0700

    kd

--- main.c
+++ main.c
@@ -3,42 +3,43 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * kd_main - Entry point
  *
  * Return: Always 0
  */
-int main(void)
+int kd_main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+    int kd_len;
+    int kd_len2;
+    unsigned int kd_ui;
+    void *kd_addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
+    kd_len = kd_printf("Let's try to printf a simple sentence.\n");
+    kd_len2 = printf("Let's try to printf a simple sentence.\n");
+    kd_ui = (unsigned int)INT_MAX + 1024;
+    kd_addr = (void *)0x7ffe637541f0;
+    kd_printf("Length:[%d, %i]\n", kd_len, kd_len);
+    printf("Length:[%d, %i]\n", kd_len2, kd_len2);
+    kd_printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
+    kd_printf("Unsigned:[%u]\n", kd_ui);
+    printf("Unsigned:[%u]\n", kd_ui);
+    kd_printf("Unsigned octal:[%o]\n", kd_ui);
+    printf("Unsigned octal:[%o]\n", kd_ui);
+    kd_printf("Unsigned hexadecimal:[%x, %X]\n", kd_ui, kd_ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", kd_ui, kd_ui);
+    kd_printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
+    kd_printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
+    kd_printf("Address:[%p]\n", kd_addr);
+    printf("Address:[%p]\n", kd_addr);
+    kd_len = kd_printf("Percent:[%%]\n");
+    kd_len2 = printf("Percent:[%%]\n");
+    kd_printf("Len:[%d]\n", kd_len);
+    printf("Len:[%d]\n", kd_len2);
+    kd_printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
-    return (0);
+    return (0);
 }
+
